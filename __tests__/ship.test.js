79912d24a51c0d81f6549b71903e3990a1c42c00
Ian King <ian.antking@gmail.com>
2018-09-19 19:12:40 +0100

    Added mocks and stubs to ship.test.js Tests still pass

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,25 +1,44 @@
 const Ship = require('../src/ship.js');
-const Port = require('../src/port.js');
 const Itinerary = require('../src/itinery.js');
 
 let ship = null;
+let port = null;
+let itinerary = null;
 let london = null;
 let newYork = null;
-let itinerary = null;
 let shanghai = null;
 
 beforeEach(() => {
-    london = new Port('London');
-    newYork = new Port('New York');
-    shanghai = new Port('Shanghai')
+    port = {
+        addShip: jest.fn(),
+        removeShip: jest.fn(),
+    };
+    london = {
+        ...port,
+        name: 'london',
+        ships: [],
+    };
+    newYork = {
+        ...port,
+        name: 'New York',
+        ships: [],
+    };
+    shanghai = {
+        ...port,
+        name: 'Shanghai',
+        ships: [],
+    };
+    
     itinerary = new Itinerary([london, newYork, shanghai]);
     ship = new Ship(itinerary);
 });
 
-function sailDock(ship){
-    ship.sail();
-    ship.dock();
-}
+function sailDock(ship, number){
+    for(let journey = 0; journey < number; journey += 1){
+        ship.sail();
+        ship.dock();
+    };
+};
 
 it('has an itinerary property', () => {
     expect(ship.itinerary).toEqual(itinerary);
@@ -30,7 +49,7 @@ it('Ship is an object with a current port property', () => {
 });
 
 it('ship gets added to port.ships on instantiation', () => {
-    expect(ship.currentPort.ships).toContain(ship);
+    expect(port.addShip).toHaveBeenCalledWith(ship);
 });
 
 it('has a previous port property at construction', () => {
@@ -41,21 +60,18 @@ it('Ship is able to set sail', () => {
     ship.sail();
     expect(ship.currentPort).toBeFalsy();
     expect(ship.previousPort).toBe(london);
-    expect(ship.previousPort.ships.indexOf(ship)).toBe(-1);
+    expect(london.ships.indexOf(ship)).toBe(-1);
 })
 
 it('ship is able to dock at port', () => {
-    sailDock(ship);
+    sailDock(ship, 1);
     expect(ship.currentPort).toBe(newYork);
-    sailDock(ship);
+    sailDock(ship, 1);
     expect(ship.currentPort).toBe(shanghai);
 });
 
 it('throws an error if ship tries to sail past it\'s itinerary', () => {
-    for(let journey = 0; journey < itinerary.ports.length; journey += 1){
-        ship.sail();
-        ship.dock();  
-    }
+    sailDock(ship, itinerary.ports.length);  
     expect(ship.sail).toThrow(Error('Ship has completed itinerary'));
 });
 
