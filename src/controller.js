a2115910836bc87c094cfc2d218022e288a605be
Ian King <ian.antking@gmail.com>
2018-10-08 21:21:53 +0100

    Used controller to animate ocean background

--- src/controller.js
+++ src/controller.js
@@ -2,7 +2,12 @@ const Controller = function Controller() {
   this.initialiseSea();
 };
 
-Controller.prototype.initialiseSea = function initialiseSea(){
-
+Controller.prototype.initialiseSea = function initialiseSea() {
+  const viewport = document.querySelector('#viewport');
+  const backgrounds = ["url('./images/water0.png')", "url('./images/water1.png')"];
+  let backgroundIndex = 0;
+  setInterval(() => {
+    viewport.style.backgroundImage = backgrounds[backgroundIndex % backgrounds.length];
+    backgroundIndex += 1;
+  }, 1000);
 };
-
